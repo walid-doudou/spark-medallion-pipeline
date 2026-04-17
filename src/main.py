9978576d26f428b4cbddc20f892030a8972a50ba
walid.doudou.externe@edf-power.com <walid.doudou.externe@edf-power.com>
2026-04-17 18:39:43 +0200

    fix: type hint

--- src/main.py
+++ src/main.py
@@ -2,16 +2,17 @@ from bronze import bronze_run
 from config import get_spark_session
 
 
-class Perform():
-    """Run the process 
-    """
-    
-    def __init__(self):
+class Perform:
+    """Run the process"""
+
+    def __init__(self) -> None:
         self.spark = get_spark_session()
         self.run()
-        
-    def run(self):
-        bronze = bronze_run(self.spark)
-        #silver = silver_run(self.spark)
-        #gold = gold_run(self.spark)
-    
+
+    def run(self) -> None:
+        bronze_run(self.spark)
+        # silver = silver_run(self.spark)
+        # gold = gold_run(self.spark)
+
+
+Perform()
